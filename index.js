ff10f1bcd6338eed10d1be69b90ec76568921073
mattenbar <matthw.enbar@gmail.com>
2020-07-24 14:24:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, func){
+  let r = []
+  for (let i = 0; i < arr.length; i++){
+    r.push(func(arr[i]))
+  }
+  return r 
+}
+
+function reduce(src, conditions, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = conditions(src[i], r)
+  }
+
+  return r;
+}
